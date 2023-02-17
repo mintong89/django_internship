db6c77fc3818e798b360a84ce5d219d187e6884a
mintong89 <caferatte89@hotmail.com>
2023-02-17 17:51:07 +0800

    add register author

--- old_straits_times/views.py
+++ old_straits_times/views.py
@@ -4,9 +4,13 @@ from django.http import HttpResponseRedirect
 from django.utils import timezone
 from django.urls import reverse
 
+from django.core.validators import validate_email
+from django.contrib.auth.password_validation import validate_password
+
 from .models import Story, Genre, Author
 
 import logging
+import re
 
 # Create your views here.
 
@@ -54,9 +58,16 @@ def story(request, story_id):
 def auth_login(request):
     template_name = 'old_straits_times/login.html'
     
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('oldstimes:index'))
+    
     context = {
+        'success_message': '',
         'error_message': ''
     }
+    
+    if request.GET.get('m', '') == 'reg_success':
+        context['success_message'] = 'Register sucessfully! Please login your account.'
     
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -74,6 +85,71 @@ def auth_logout(request):
     logout(request)
     
     return HttpResponseRedirect(reverse('oldstimes:index'))
+
+def register(request):
+    template_name = 'old_straits_times/register.html'
+    
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('oldstimes:index'))
+    
+    if request.method == 'POST':
+        first_name:str = request.POST.get('first_name')
+        last_name:str = request.POST.get('last_name')
+        username:str = request.POST.get('username')
+        email:str = request.POST.get('email')
+        password:str = request.POST.get('password')
+        confirmation_password:str = request.POST.get('confirmation_password')
+        
+        # check username
+        if not re.match(r'[a-zA-Z0-9@.+-_]{8,20}', username):
+            return render(request, template_name, {
+                'error_message': "The username does not meet criteria!"
+            })
+            
+        if Author.objects.filter(username=username).exists():
+            return render(request, template_name, {
+                'error_message': "The username has been used! Try another username."
+            })
+        
+        # check email address
+        try:
+            validate_email(email)
+        except:
+            return render(request, template_name, {
+                'error_message': "Invalid email address!"
+            })
+            
+        if Author.objects.filter(email=email).exists():
+            return render(request, template_name, {
+                'error_message': "The email address has been used!"
+            })
+        
+        # check password
+        try:
+            validate_password(password)
+        except:
+            return render(request, template_name, {
+                'error_message': "Invalid password!"
+            })
+        
+        # check confirmation password
+        if confirmation_password != password:
+            return render(request, template_name, {
+                'error_message': "Password not match!"
+            })
+            
+        new_author = Author.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email
+        )
+        new_author.set_password(password)
+        new_author.save()
+        
+        return HttpResponseRedirect('%s?m=reg_success' % reverse('oldstimes:auth_login'))
+        
+    return render(request, template_name, {})
 
 def profile(request, author_username):
     template_name = 'old_straits_times/profile.html'
